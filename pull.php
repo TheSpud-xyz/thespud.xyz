0a8458eb412e953f6769c2cd6343ec15b9107a7f
zacnoo <zacnoo@gmail.com>
2020-04-08 16:01:16 +1200

    Test push 3

--- pull.php
+++ pull.php
@@ -1,5 +1,6 @@
 <?php
+echo "Test push 3";
 if ($_POST['payload']) {
-shell_exec('cd /var/www/html/ && git reset --hard MASTER && git pull');
+shell_exec('cd /var/www/html/ && git reset --hard HEAD && git pull');
 }
 ?>
