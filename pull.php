ec26db41dc06ca443a035e232dbd40babef33f72
Zack Palmer <zacnoo@gmail.com>
2020-04-08 03:54:06 +0000

    got pull.php working (i think)

--- pull.php
+++ pull.php
@@ -1,8 +1,6 @@
 <?php
-// Use in the “Post-Receive URLs” section of your GitHub repo.
-shell_exec( 'touch itsworking' );
-if ( $_POST['payload'] ) {
-shell_exec( ‘cd /var/www/html/ && git reset –hard HEAD && git pull && touch itsworking’);
-}
 
-?>hi
+if ($_POST['payload']) {
+shell_exec('cd /var/www/html/ && git reset -hard && git pull');
+}
+?>
