15d6f9fe443c31e2f30c161721bab6835a7e267b
Harry Atkinson <hazzard1579@gmail.com>
2020-04-10 00:29:33 +1200

    Make minecraft nice

--- js/minecraftServerStatus.js
+++ js/minecraftServerStatus.js
@@ -1,6 +1,7 @@
 function checkServer(){
     //based on a pen by @robinselmer
-    var url = "https://mcapi.us/server/status?ip=thespud.xyz&port=25565";
+    var url = "https://api.mcsrvstat.us/2/thespud.xyz:25567";
+    var playerList = "";
     //https://mcapi.us/server/status?ip=666.fluctis.com&port=25757
     $.getJSON(url, function(r) {
         //data is the JSON string
@@ -8,10 +9,14 @@ function checkServer(){
         $('#rest').html('Can\'t Reach Server');
        return false;
      }
-     console.log(r.motd);
-     $('#rest').html(r.motd);
-     $('#players').html(r.players.now + '/' + r.players.max);
-     $('#serverIcon').attr('src', r.favicon);
+     $('.serverTitle').html("<p>" + r.hostname + "</p>")
+     $('#rest').html(r.motd.html);
+     $('#players').html(r.players.online + '/' + r.players.max);
+     $('#serverIcon').attr('src', r.icon);
+     if (r.players.list != null) {
+         $('.innerPlayersOnline').html(r.players.list)
+     }
+
     });
 
 }
