b5f11c7871c9e36dfac4f319a87f0862edbb893d
Harry Atkinson <hazzard1579@gmail.com>
2020-04-10 10:28:05 +1200

    More Mcserver things

--- js/minecraftServerStatus.js
+++ js/minecraftServerStatus.js
@@ -1,20 +1,23 @@
 function checkServer(){
     //based on a pen by @robinselmer
-    var url = "https://api.mcsrvstat.us/2/thespud.xyz:25567";
+    var url = "https://api.mcsrvstat.us/2/thespud.xyz";
     var playerList = "";
     //https://mcapi.us/server/status?ip=666.fluctis.com&port=25757
     $.getJSON(url, function(r) {
         //data is the JSON string
-     if(r.error){
+     if(r.online = false){
         $('#rest').html('Can\'t Reach Server');
        return false;
      }
+
      $('.serverTitle').html("<p>" + r.hostname + "</p>")
      $('#rest').html(r.motd.html);
      $('#players').html(r.players.online + '/' + r.players.max);
      $('#serverIcon').attr('src', r.icon);
      if (r.players.list != null) {
-         $('.innerPlayersOnline').html(r.players.list)
+         for (var i = 0; i < r.players.list.length; i++) {
+             $('.innerPlayersOnline').append(r.players.list[i] + ", ")
+         }
      }
 
     });
