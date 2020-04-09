ffa088467342c9e5b1db1e0d57585c19d4bbdc9f
Zack Palmer <zacnoo@gmail.com>
2020-04-10 00:06:08 +1200

    Revert "Secret testing"
    This reverts commit 86ccbd01

--- pull.php
+++ pull.php
@@ -1,6 +1,5 @@
 <?php
-if ($_POST['payload'].secret === "acersucks") {
-echo $_POST['payload'].secret;
+if ($_POST['payload']) {
 $shell_response = `sh /var/www/html/git_pull.sh`;
 echo "$shell_response";
 }
